196a9eafab47f7983073efc8258f592eecb5caab
Mohit Singh <unignoramus@gmail.com>
2024-09-16 23:21:26 +0530

    Update backend code

--- node/data_collection.cpp
+++ node/data_collection.cpp
@@ -9,11 +9,7 @@
 #define WiFi_SSID "SSID"
 #define WiFi_PASS "PASSWORD"
 
-#define BUZZER_PIN 22
-
-#define LED_R 21
-#define LED_G 19
-#define LED_B 18
+#define LED_BUILTIN 2
 
 Adafruit_MPU6050 mpu;
 
@@ -23,137 +19,29 @@ WiFiClient client;
 unsigned long myChannelNumber = 123456789;
 const char *myWriteAPIKey = "WRITE_API_KEY";
 
-void success()
-{
-    for (int i = 0; i < 256; i++)
-    {
-        analogWrite(LED_R, i);
-        analogWrite(LED_G, 255 - i);
-        analogWrite(LED_B, 255 - i);
-        delay(5);
-    }
-
-    for (int i = 0; i < 256; i++)
-    {
-        analogWrite(LED_R, 255 - i);
-        analogWrite(LED_G, i);
-        analogWrite(LED_B, 255 - i);
-        delay(5);
-    }
-
-    for (int i = 0; i < 256; i++)
-    {
-        analogWrite(LED_R, 255 - i);
-        analogWrite(LED_G, 255 - i);
-        analogWrite(LED_B, i);
-        delay(5);
-    }
-
-    analogWrite(LED_R, 0);
-    analogWrite(LED_G, 0);
-    analogWrite(LED_B, 0);
-}
-
-void connectingToNetwork()
-{
-    for (int i = 0; i < 256; i++)
-    {
-        analogWrite(LED_R, i);
-        analogWrite(LED_G, i);
-        analogWrite(LED_B, 0);
-        delay(5);
-    }
-}
-
-void connectedToNetwork()
-{
-    for (int i = 0; i < 256; i++)
-    {
-        analogWrite(LED_R, 255 - i);
-        analogWrite(LED_G, 255 - i);
-        analogWrite(LED_B, 0);
-        delay(5);
-    }
-}
-
-void error()
-{
-    for (int i = 0; i < 256; i++)
-    {
-        analogWrite(LED_R, i);
-        analogWrite(LED_G, min(0, 200 - i));
-        analogWrite(LED_B, min(0, 200 - i));
-        delay(5);
-    }
-}
-
-void initializePins()
-{
-    pinMode(BUZZER_PIN, OUTPUT);
-
-    pinMode(LED_R, OUTPUT);
-    pinMode(LED_G, OUTPUT);
-    pinMode(LED_B, OUTPUT);
-
-    digitalWrite(LED_R, LOW);
-    digitalWrite(LED_G, LOW);
-    digitalWrite(LED_B, LOW);
-
-    digitalWrite(BUZZER_PIN, HIGH);
-}
-
-void connectToNetwork()
-{
-    if (WiFi.status() == WL_CONNECTED)
-        return;
-    connectingToNetwork();
-    WiFi.begin(WiFi_SSID, WiFi_PASS);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(1000);
-        Serial.print(".");
-    }
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    connectedToNetwork();
-}
-
-void thingspeakError()
-{
-    for (int i = 0; i < 256; i++)
-    {
-        analogWrite(LED_R, i);
-        analogWrite(LED_G, i / 2);
-        analogWrite(LED_B, 0);
-        delay(5);
-    }
-}
-
 void setup()
 {
-    initializePins();
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
 
     Serial.begin(115200);
     Wire.begin();
 
-    // Try to initialize MPU6050
-    if (!mpu.begin(0x69))
+    if (!mpu.begin())
     {
         Serial.println("Failed to find MPU6050 chip");
         while (1)
         {
-            delay(10);
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(100);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(100);
         }
     }
     Serial.println("MPU6050 Found!");
 
-    // Set up the accelerometer range
     mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-    // Set up the gyroscope range
     mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-    // Set filter bandwidth
     mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
     delay(100);
@@ -165,7 +53,37 @@ void setup()
     delay(2000);
 }
 
+void connectToNetwork()
+{
+    WiFi.begin(WiFi_SSID, WiFi_PASS);
+    Serial.print("Connecting to WiFi");
+
+    unsigned long startAttemptTime = millis();
+    while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 10000)
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(100);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(100);
+        Serial.print(".");
+    }
+
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        Serial.println("\nWiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else
+    {
+        Serial.println("\nFailed to connect to WiFi");
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+}
+
 int counter = 0;
+float highest_value = 0;
 
 void loop()
 {
@@ -184,10 +102,12 @@ void loop()
     Serial.print(a.acceleration.z);
     Serial.println(" m/s^2");
 
-    float val = pow(pow(a.acceleration.x, 2) + pow(a.acceleration.y, 2) + pow(a.acceleration.z, 2), 0.5);
-    Serial.print("\tSpecial Value ;) : ");
-    Serial.print(val);
-    Serial.println("m/s^2");
+    float accelMagnitude = sqrt(pow(a.acceleration.x, 2) + pow(a.acceleration.y, 2) + pow(a.acceleration.z, 2));
+    Serial.print("\tAcceleration Magnitude: ");
+    Serial.print(accelMagnitude);
+    Serial.println(" m/s^2");
+
+    highest_value = max(highest_value, accelMagnitude);
 
     Serial.print("\tRotation X: ");
     Serial.print(g.gyro.x);
@@ -203,10 +123,10 @@ void loop()
 
     Serial.println("");
 
-    if (counter++ == 60)
+    if (counter++ == 300)
     {
-        counter = 0;
-        connectToNetwork();
+        if (WiFi.status() != WL_CONNECTED)
+            connectToNetwork();
 
         ThingSpeak.setField(1, a.acceleration.x);
         ThingSpeak.setField(2, a.acceleration.y);
@@ -215,22 +135,24 @@ void loop()
         ThingSpeak.setField(5, g.gyro.y);
         ThingSpeak.setField(6, g.gyro.z);
         ThingSpeak.setField(7, temp.temperature);
-        // ThingSpeak.setField(8, val);
-        ThingSpeak.setField(8, 42);
+        ThingSpeak.setField(8, highest_value);
 
         int code = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
 
         if (code == 200)
         {
             Serial.println("Channel update successful.");
-            success();
+            digitalWrite(LED_BUILTIN, HIGH);
         }
         else
         {
             Serial.println("Problem updating channel. HTTP error code " + String(code));
-            thingspeakError();
+            digitalWrite(LED_BUILTIN, LOW);
         }
+
+        counter = 0;
+        highest_value = 0;
     }
 
-    delay(500);
+    delay(50);
 }
